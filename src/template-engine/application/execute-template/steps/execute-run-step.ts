e7fcadd952811fe8e04c14a63cf4a95463379f32
Loris Rüegg <94956347+LorisRue@users.noreply.github.com>
2026-05-20 10:02:05 +0200

    feat: add and implement verbosity option to run step (#78)

--- src/template-engine/application/execute-template/steps/execute-run-step.ts
+++ src/template-engine/application/execute-template/steps/execute-run-step.ts
@@ -2,6 +2,7 @@ import type { VariableValue } from "@/template-engine/domain/variable-value";
 import type { RunStep } from "@/template-engine/domain";
 import { replaceVariablesInString } from "../replace-variable-in-string";
 import type { RunCommandPort } from "@/template-engine/domain/ports/run-command.port";
+import { ProjgenError } from "@/shared";
 
 export const executeRunStep = async (
   step: RunStep,
@@ -15,5 +16,16 @@ export const executeRunStep = async (
     ? replaceVariablesInString(step.cwd, variables)
     : undefined;
 
-  await runCommand(command, args, cwd);
+  const verbosity = replaceVariablesInString(
+    step.verbosity ?? "all",
+    variables,
+  );
+
+  if (!["all", "warning", "none"].includes(verbosity)) {
+    throw new ProjgenError(
+      `Invalid verbosity level "${verbosity}" in step "${step.name}". Valid options are "all", "warning", or "none".`,
+    );
+  }
+
+  await runCommand(command, args, cwd, verbosity as "all" | "warning" | "none");
 };
